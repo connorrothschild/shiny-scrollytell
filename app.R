549b8df705c8cf04878459af730634afbac6cd73
Connor Rothschild <connorrothschild@gmail.com>
2019-10-25 15:54:10 -0500

    rename and such

--- app.R
+++ app.R
@@ -1,116 +1,132 @@
 library(shiny)
+library(scrollytell)
 library(shinyjs)
 library(ggvis)
 library(waypointer)
 library(shticky)
 
-source("initial_source_code_for_shiny.R")
+cr::set_cr_theme()
 
-OFFSET <- "0"
-ANIMATION <- "slideInUp"
+source("source_code_for_shiny.R")
+
+# Define UI
 
 ui <- fluidPage(
+  
+  # Code to suppress warning messages while data is loading on-screen 
+  # reference (https://groups.google.com/forum/#!topic/shiny-discuss/FyMGa2R_Mgs)
+  tags$style(type="text/css",
+             ".shiny-output-error { visibility: hidden; }",
+             ".shiny-output-error:before { visibility: hidden; }"),
   tags$head(
-    includeCSS("www/initial_style.css")
+    includeCSS("www/style.css")
   ),
-  use_shticky(),
-  use_waypointer(),
-  div(
-    div(
-      id = "stick",
-      style = "position:relative;width:100%;height:600px;",
-      fluidRow(
-        column(width = 12,
-               ggvisOutput("plot")
-               )
-      )
+  
+  # Article title
+  fluidRow(HTML("<center>
+                <h1>Automation and Its Impact on Jobs</h1>
+                <p style='size:18px';> by <a href='https://connorrothschild.github.io/' target='_blank'>Connor Rothschild</a></p>
+                </center>")
+  ),
+  br(),
+  
+  # plot object for intro
+  fluidRow(
+    column(1),
+    column(10,
+           # Introduction
+           fluidRow(id='text',
+                    column(1),
+                    column(10, 
+                           br(),
+                           text0,
+                           br(),
+                           # ggvisOutput("interactivePlot"),
+                           plotOutput("introPlot", height = '600px')
+                    ),
+                    column(1)
+           ),
+           
+           br(), 
+           br()
+           )
     ),
-    longdiv(
-      h1("Automation and Its Impact on Jobs", class = "title"),
-      h1(
-        class = "subtitle",
-        "How do different occupations differ in their susceptibility to automation?"
-      )
-    ),
-  longdiv(
-    div(
-      id = "m1",
-      uiOutput("1")
-    )
+  
+  # scrollytelling plot
+  scrolly_container("scr"
+                    , scrolly_graph( br(), 
+                                     br(),
+                                     textOutput("section"),
+                                     br(),
+                                     HTML('<center>'),
+                                     plotOutput("plot", height = '600px'),
+                                     HTML('</center>')
+                                     
+                    )
+                    , scrolly_sections(
+                      HTML('<center>'),
+                      scrolly_section(id = 0, render_text(0)),
+                      scrolly_section(id = 1, render_text(1)),
+                      scrolly_section(id = 2, render_text(2)),
+                      scrolly_section(id = 3, render_text(3)),
+                      scrolly_section(id = 4, render_text(4)),
+                      scrolly_section(id = 5, render_text(5)),
+                      scrolly_section(id = 6, render_text(6)),
+                      scrolly_section(id = 7, render_text(7)),
+                      scrolly_section(id = 8, render_text(8)),
+                      # add a scrolly_section with nothing in it; 
+                      # this buffer prevents the plot from disappearing while reading last section
+                      scrolly_section(id = "buffer", br()),
+                      HTML('</center>')
+                    )
+                    
   ),
-  longdiv(
-    div(
-      id = "m2",
-      uiOutput("2")
-    )
+  
+  # Concluding text
+  div(fluidRow(id = 'text',
+               column(2),
+               column(8, 
+                      br(),
+                      HTML("<p><span style='font-size:30px'><b>The tournament</b></span>. As expected, once the men's Olympic basketball competition began, there was only one team that could leave Barcelona with the gold medal.  The United States cruised through all eight of its contests. In fact, during several games, it appeared as though the opposing team was just happy to be on the same floor as the NBA players.<br><br>
+                                
+                                Team USA averaged 117 points per game and held opponents to a measly 73 points per game over the two-week stretch.  Charles Barkley (18 PPG) and Michael Jordan (14.9 PPG) led the way in scoring.<br><br>
+                                Below is a look at player contributions in each of their eight victories.</p>"),
+                      br()
+               ),
+               column(2)
+  ), style = 'margin-top: -300px;'),
+  
+  br(),
+  br(),
+  br(),
+  
+  fluidRow(id = 'text',
+           column(2),
+           column(8, 
+                  br(),
+                  HTML("<p><span style='font-size:30px'><b>The impact</b></span>. The Dream Team's gold medal finish at the 1992 Olympics avenged a third place finish in 1988 and put the USA back on top of the basketball world - by a large margin. NBA popularity skyrocketed around the globe, thanks in part to the larger-than-life personalities of superstars like Michael Jordan, Magic Johnson, and Charles Barkley.<br><br>
+                                    
+                                    Team USA's decisive victory sent a message to the rest of the world that in order to compete for a championship on the global stage, huge improvements in skill and player development was essential. The world responded.  By 2004, international teams and players had elevated their games so much so that team USA settled for the bronze medal that year.<br><br>
+                                    
+                                    Since the 1992 games, numerous international players (such as Dirk Nowitzki, Manu Ginobili, Tony Parker, Pau and Marc Gasol) have come to the NBA, won championships, and found great success. While team USA is still a major favorite to stay on top, the Dream Team's influence on the international competition has ensured that this will be no easy task.<br><br>
+                                    
+                                    The game of basketball is better for it. </p>"),
+                  br()
+           ),
+           column(2)
   ),
-  longdiv(
-    div(
-      id = "m3",
-      uiOutput("3")
-    )
-  ),
-  longdiv(
-    div(
-      id = "m4",
-      uiOutput("4")
-    )
-  ),
-  longdiv(
-    div(
-      id = "m5",
-      uiOutput("5")
-    )
-  ),
-  longdiv(
-    div(
-      id = "m6",
-      uiOutput("6")
-    )
-  ),
-  longdiv(
-    div(
-      id = "m7",
-      uiOutput("7")
-    )
-  ),
-  longdiv(
-    div(
-      id = "m8",
-      uiOutput("8")
-    )
-  ),
-  longdiv(
-    id = "m9",
-    uiOutput("9")
-  )
-  ),
-  longdiv(
-    id = "m10",
-    h1("Thank you!", class = "title"),
-    br(),
-    br(),
-    div(
-    tags$a("Blog",
-      href = "https://connorrothschild.github.io", class = "subtitle"
-    ),
-    br(),
-    tags$a("Twitter",
-      href = "https://twitter.com/CL_Rothschild", class = "subtitle"
-    ),
-    br(),
-    tags$a("GitHub",
-      href = "https://github.com/connorrothschild/", class = "subtitle"
-    ), style = "text-align:center;"
-    ),
-    br(),
+  br(),
+  br(),
+  hr(),
+  br(),
+  
   fluidRow(
     column(2),
     column(8,
            HTML("<p>
-                <span style='font-size:18px'><i>Technical Notes</i></span><br>
+                <span style='font-size:18px'><i><u>Technical Notes</u></i></span><br>
                 <br>
-                <span style='font-size:14px'>
+                <span style='font-size:12px'>
                 Employment and education data comes from the
                 <a href='https://www.bls.gov/emp/documentation/education-training-system.htm' target='_blank'>Bureau of Labor Statistics</a>. 
                 <br>
@@ -140,160 +156,51 @@ ui <- fluidPage(
                 </p>")
     ),
     column(2)
-  )
-  )
+  ),
+  br(),
+  br(),
+column(1)
+
 )
 
+# Define server logic
 server <- function(input, output, session) {
   
-  w1 <- Waypoint$
-    new("m1", offset = OFFSET, animate = FALSE)$
-    start()
-  w2 <- Waypoint$
-    new("m2", offset = OFFSET, animate = FALSE)$
-    start()
-  w3 <- Waypoint$
-    new("m3", offset = OFFSET, animate = FALSE)$
-    start()
-  w4 <- Waypoint$
-    new("m4", offset = OFFSET, animate = FALSE)$
-    start()
-  w5 <- Waypoint$
-    new("m5", offset = OFFSET, animate = FALSE)$
-    start()
-  w6 <- Waypoint$
-    new("m6", offset = OFFSET, animate = FALSE)$
-    start()
-  w7 <- Waypoint$
-    new("m7", offset = OFFSET, animate = FALSE)$
-    start()
-  w8 <- Waypoint$
-    new("m8", offset = OFFSET, animate = FALSE)$
-    start()
-  w9 <- Waypoint$
-    new("m9", offset = OFFSET, animate = FALSE)$
-    start()
-  
-  output$`1` <- renderUI({
-    # req(w1$get_triggered())
-    # if(w1$get_triggered() == TRUE) 
-      # render_text(1)
-  })
-  
-  output$`2` <- renderUI({
-    # req(w2$get_triggered())
-    # if(w2$get_triggered() == TRUE) 
-      render_text(1)
-  })
-  
-  output$`3` <- renderUI({
-    # req(w3$get_triggered())
-    # if(w3$get_triggered() == TRUE) 
-      render_text(2)
-  })
-  
-  output$`4` <- renderUI({
-    # req(w4$get_triggered())
-    # if(w4$get_triggered() == TRUE) 
-      render_text(3)
-  })
-  
-  output$`5` <- renderUI({
-    # req(w5$get_triggered())
-    # if(w5$get_triggered() == TRUE) 
-      render_text(4)
-  })
-  
-  output$`6` <- renderUI({
-    # req(w6$get_triggered())
-    # if(w6$get_triggered() == TRUE) 
-      render_text(5)
-  })
-  
-  output$`7` <- renderUI({
-    # req(w7$get_triggered())
-    # if(w7$get_triggered() == TRUE) 
-      render_text(6)
-  })
-  
-  output$`8` <- renderUI({
-    # req(w8$get_triggered())
-    # if(w8$get_triggered() == TRUE) 
-      render_text(7)
-  })
-  
-  output$`9` <- renderUI({
-    # req(w8$get_triggered())
-    # if(w8$get_triggered() == TRUE) 
-    render_text(8)
-  })
-  
-  # Our sticky plot
-  shtick <- Shtick$
-    new("#stick")$
-    shtick()
-
-  vis <-  reactive({
- 
+  output$plot <- renderPlot({
+    
+    add <- input$scr
+    
+    legend_ord <- levels(with(data, reorder(typicaled, reveal)))
+    
     data %>% 
-      filter(typicaled != "Some college, no degree") %>% 
-      ggvis(~A_MEDIAN, ~probability, opacity := 0.5, key := ~occupation) %>% 
-      scale_numeric('size',domain = c(100000,500000),range=c(100,500)) %>%
-      layer_points(fill = ~typicaled, size = ~TOT_EMP) %>% 
-      add_tooltip(function(data) glue::glue('Occupation: {data$occupation}',
-                                             'Number of Workers: {scales::comma(data$TOT_EMP)}',
-                                             'Probability of Automation: {data$probability}%',
-                                            'Income: {scales::dollar(data$A_MEDIAN)}',
-                                             .sep = "<br />")) %>% 
-      add_axis("x", title = "Median Income", grid = FALSE) %>%
-      add_axis("y", title = "Probability of Automation", title_offset = 50, grid = FALSE) %>% 
-      add_legend("fill", title = "Education", properties = legend_props(legend = list(y = 200))) %>%
-      add_legend("size", title = "Number of Workers", properties = legend_props(legend = list(y = 50))) %>%
-      scale_numeric("x", domain = c(25000, 200000), nice = FALSE) %>%
-      scale_numeric("y", domain = c(0, 100), nice = FALSE) %>% 
-      scale_nominal("fill", 
-                    domain = c('No formal educational credential','High school diploma or equivalent', "Postsecondary nondegree award",
-                               "Associate's degree", "Bachelor's degree", "Master's degree", "Doctoral or professional degree"), 
-                    range = c('#A00042', '#F56C42', '#AADDA3', '#3487BD', '#5E4FA2', '#C71C7E', "#1A1A1A")) %>% 
-      set_options(duration = 0, width = "auto", height = "auto", resizable = FALSE)
+      filter(typicaled != "Some college, no degree") %>%
+      filter(if (add != 8) add >= reveal else reveal %in% c(1:8)) %>%
+      ggplot() +
+      geom_point(mapping=aes(x=A_MEDIAN, y=probability, size=TOT_EMP,
+                             alpha=ifelse(add == reveal, 1/5, 1/10), col=typicaled))+
+      scale_size(range = c(1, 20), guide = 'none') +
+      xlab("\nMedian Income") +
+      ylab("Probability of Automation") +
+      # ggtitle("Likelihood of Job Automation vs Median Income") +
+      labs(size=element_blank(), col=element_blank()) +
+      labs(alpha=NULL) +
+      guides(alpha=FALSE) +
+      scale_color_manual(values = cols, breaks = legend_ord) +
+      scale_x_continuous(labels=scales::dollar_format(prefix="$"), limits = c(25000,200000)) +
+      scale_y_continuous(labels=scales::number_format(suffix="%"), limits = c(0,100)) +
+      theme(legend.position = "top", legend.direction = "horizontal") +
+            # legend.text = element_text(colour = ifelse(add == reveal, "black", "grey"))) +
+            # legend.text = element_text(colour="black", size = ifelse(add == reveal, 20, 12))) +
+      cr::drop_axis(axis = "y")
     
   })
+    
+  # output$interactivePlot <- vis %>% bind_shiny("vis")
+  output$introPlot <- renderPlot({introPlot})
+  output$scr <- renderScrollytell({scrollytell()})
+  renderText(paste0("Section: ", input$scr))
+  observe({cat("section:", input$scr, "\n")})
   
-output$plot <-
-   vis %>% bind_shiny("plot")
-
-observeEvent(w1$get_direction(), {
-  if(w1$get_direction() == "down") add_data(1)
-})
-
-observeEvent(w2$get_direction(), {
-  if(w2$get_direction() == "down") add_data(2)
-})
-
-observeEvent(w3$get_direction(), {
-  if(w3$get_direction() == "down") add_data(3)
-})
-
-observeEvent(w4$get_direction(), {
-  if(w4$get_direction() == "down") add_data(4)
-})
-
-observeEvent(w5$get_direction(), {
-  if(w5$get_direction() == "down") add_data(5)
-})
-
-observeEvent(w6$get_direction(), {
-  if(w6$get_direction() == "down") add_data(6)
-})
-
-observeEvent(w7$get_direction(), {
-  if(w7$get_direction() == "down") add_data(7)
-})
-
-observeEvent(w8$get_direction(), {
-  if(w8$get_direction() == "down") add_data(8)
-})
-
-} 
-
-shinyApp(ui, server)
+}
+# Run the application
+shinyApp(ui = ui, server = server)
